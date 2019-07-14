81eba3db9806f6f7f2fa30448555deac06951bf0
Sanjeevan-Tal <sanjeevan.biswas@talentica.com>
2019-07-14 11:17:02 +0530

    Added test cases

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ npm install
 
 #### Start web server
 npm run start
+
+#### Test Application
+npm run test
+
+Test result will be generated as html mochawesome-report/mochawesome.html and json mochawesome-report/mochawesome.json
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import assert from'assert';
-import zxcvbn from './static/zxcvbn.js';
+import zxcvbn from './node_modules/zxcvbn/dist/zxcvbn.js';
 import RegistrationForm from './static/main.js';
 
 
@@ -106,10 +106,190 @@ describe('Validate all number Password: "12345678"', () => {
     });
 });
 
+describe('Validate Password with length less than 8: "A_eIoU0"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('A_eIoU0', zxcvbn);
+    
+    it('isValidPassword --> false', () => {
+        assert.equal(isValidPassword, false);
+    });
+
+    it('score --> 0', () => {
+        assert.equal(score, 0);
+    });
+
+    it('Password should be of 8+ characters. --> false', () => {
+        assert.equal(acceptanceCriteria.length, false);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.lowercase, true);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.uppercase, true);
+    });
+
+    it('Password should contain atleast 1 number. --> true', () => {
+        assert.equal(acceptanceCriteria.number, true);
+    });
+
+    it('Password should contain atleast 1 special character. --> true', () => {
+        assert.equal(acceptanceCriteria.special, true);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
+describe('Validate Password with no lowercases: "NO_LOWER_CASES-1"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('NO_LOWER_CASES-1', zxcvbn);
+    
+    it('isValidPassword --> false', () => {
+        assert.equal(isValidPassword, false);
+    });
+
+    it('score --> 0', () => {
+        assert.equal(score, 0);
+    });
+
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> false', () => {
+        assert.equal(acceptanceCriteria.lowercase, false);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.uppercase, true);
+    });
+
+    it('Password should contain atleast 1 number. --> true', () => {
+        assert.equal(acceptanceCriteria.number, true);
+    });
+
+    it('Password should contain atleast 1 special character. --> true', () => {
+        assert.equal(acceptanceCriteria.special, true);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
+describe('Validate Password with no uppercase: "no_uppercase-1"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('no_uppercase-1', zxcvbn);
+    
+    it('isValidPassword --> false', () => {
+        assert.equal(isValidPassword, false);
+    });
+
+    it('score --> 0', () => {
+        assert.equal(score, 0);
+    });
+
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.lowercase, true);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> false', () => {
+        assert.equal(acceptanceCriteria.uppercase, false);
+    });
+
+    it('Password should contain atleast 1 number. --> true', () => {
+        assert.equal(acceptanceCriteria.number, true);
+    });
+
+    it('Password should contain atleast 1 special character. --> true', () => {
+        assert.equal(acceptanceCriteria.special, true);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
+describe('Validate Password with no number: "NO_Number-case"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('NO_Number-case', zxcvbn);
+    
+    it('isValidPassword --> false', () => {
+        assert.equal(isValidPassword, false);
+    });
+
+    it('score --> 0', () => {
+        assert.equal(score, 0);
+    });
+
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.lowercase, true);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.uppercase, true);
+    });
+
+    it('Password should contain atleast 1 number. --> false', () => {
+        assert.equal(acceptanceCriteria.number, false);
+    });
+
+    it('Password should contain atleast 1 special character. --> true', () => {
+        assert.equal(acceptanceCriteria.special, true);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
+describe('Validate Password with no special char: "NoSpecialChar1"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('NoSpecialChar1', zxcvbn);
+    
+    it('isValidPassword --> false', () => {
+        assert.equal(isValidPassword, false);
+    });
+
+    it('score --> 0', () => {
+        assert.equal(score, 0);
+    });
+
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.lowercase, true);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.uppercase, true);
+    });
+
+    it('Password should contain atleast 1 number. --> true', () => {
+        assert.equal(acceptanceCriteria.number, true);
+    });
+
+    it('Password should contain atleast 1 special character. --> false', () => {
+        assert.equal(acceptanceCriteria.special, false);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
 describe('Validate bad strength Password: "Password-1"', () => {
     let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('Password-1', zxcvbn);
     
-    it('isValidPassword --> false', () => {
+    it('isValidPassword --> true', () => {
         assert.equal(isValidPassword, true);
     });
 
@@ -142,19 +322,91 @@ describe('Validate bad strength Password: "Password-1"', () => {
     });
 });
 
-describe('Validate Password with length less than 8: "A_eIoU0"', () => {
-    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('A_eIoU0', zxcvbn);
+describe('Validate Weak strength Password: "Password-123"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('Password-123', zxcvbn);
     
-    it('isValidPassword --> false', () => {
-        assert.equal(isValidPassword, false);
+    it('isValidPassword --> true', () => {
+        assert.equal(isValidPassword, true);
     });
 
-    it('score --> 0', () => {
-        assert.equal(score, 0);
+    it('score --> 2', () => {
+        assert.equal(score, 2);
     });
 
-    it('Password should be of 8+ characters. --> false', () => {
-        assert.equal(acceptanceCriteria.length, false);
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.lowercase, true);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.uppercase, true);
+    });
+
+    it('Password should contain atleast 1 number. --> true', () => {
+        assert.equal(acceptanceCriteria.number, true);
+    });
+
+    it('Password should contain atleast 1 special character. --> true', () => {
+        assert.equal(acceptanceCriteria.special, true);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
+describe('Validate Good strength Password: "Neverforget13/3/1997"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('Neverforget13/3/1997', zxcvbn);
+    
+    it('isValidPassword --> true', () => {
+        assert.equal(isValidPassword, true);
+    });
+
+    it('score --> 3', () => {
+        assert.equal(score, 3);
+    });
+
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
+    });
+
+    it('Password should contain atleast 1 lowercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.lowercase, true);
+    });
+
+    it('Password should contain atleast 1 uppercase character. --> true', () => {
+        assert.equal(acceptanceCriteria.uppercase, true);
+    });
+
+    it('Password should contain atleast 1 number. --> true', () => {
+        assert.equal(acceptanceCriteria.number, true);
+    });
+
+    it('Password should contain atleast 1 special character. --> true', () => {
+        assert.equal(acceptanceCriteria.special, true);
+    });
+
+    it('Password strength should not be invalid or bad. --> true', () => {
+        assert.equal(acceptanceCriteria.strength, true);
+    });
+});
+
+describe('Validate Strong strength Password: "Mspt3st!F21#d"', () => {
+    let {isValidPassword, score, acceptanceCriteria} = RegistrationForm.validatePassword('Mspt3st!F21#d', zxcvbn);
+    
+    it('isValidPassword --> true', () => {
+        assert.equal(isValidPassword, true);
+    });
+
+    it('score --> 4', () => {
+        assert.equal(score, 4);
+    });
+
+    it('Password should be of 8+ characters. --> true', () => {
+        assert.equal(acceptanceCriteria.length, true);
     });
 
     it('Password should contain atleast 1 lowercase character. --> true', () => {
